dbebd5bffc0495b6eec0365771b9b52847f1e405
Jakub Miazek <the@grillazz.com>
2023-05-10 13:16:26 +0200

    gx 16.5

--- tests/api/test_expectations.py
+++ tests/api/test_expectations.py
@@ -99,7 +99,7 @@ def test_try_expectation(
                 "suite_name": "chapter_suite",
                 "suite_desc": "",
                 "value": {
-                    "meta": {"great_expectations_version": "0.16.3"},
+                    "meta": {"great_expectations_version": "0.16.5"},
                     "ge_cloud_id": None,
                     "expectations": [
                         {
