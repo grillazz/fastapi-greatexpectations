a44c696788dd86402b13d57960da18b8364bef49
Jakub Miazek <the@grillazz.com>
2023-05-15 13:11:32 +0200

    add GxSession class to keep gx session state

---
+++ app/service.py
@@ -0,0 +1,16 @@
+import great_expectations as ge
+from great_expectations.datasource.fluent.sql_datasource import SQLDatasource, TableAsset
+from great_expectations.data_context.data_context.abstract_data_context import AbstractDataContext
+# url: str = f"postgresql://user:secret@localhost:5432/gxshakezz"
+# url: str = f"postgresql://user:secret@localhost:5432/gxshakezz?options=-csearch_path=shakespeare"
+
+class GxSession:
+    # keep state of the great_expectations session
+    context: AbstractDataContext
+    sql_datasource: SQLDatasource
+    sql_table_asset: TableAsset
+
+    def __init__(self, url: str, datasource_name: str, table_name: str):
+        self.context = ge.get_context()
+        self.sql_datasource = self.context.sources.add_sql(name=datasource_name, connection_string=url)
+        self.sql_table_asset = self.sql_datasource.add_table_asset(name=f"{table_name}_asset", table_name=table_name)
