5173394e036d2456d8b833453e8d550cf73ebb95
Jakub Miazek <the@grillazz.com>
2023-05-16 12:29:38 +0200

    pass gx session to app state

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.api.v1.expectation import router as gx_router
 # from app.api.v1.validation import router as val_router
 from app.database import start_db
 from app.logging import AppLogger
+from app.service import GxSession
 
 logger = AppLogger.__call__().get_logger()
 
@@ -14,7 +15,10 @@ app.include_router(database_router)
 app.include_router(gx_router)
 # app.include_router(val_router)
 
+# TODO: build from PostgreDSN
+url: str = f"postgresql://user:secret@db:5432/gxshakezz"
 
 @app.on_event("startup")
 def startup_event():
     start_db()
+    app.state.gx = GxSession()
--- app/service.py
+++ app/service.py
@@ -4,7 +4,7 @@ from great_expectations.data_context.data_context.abstract_data_context import A
 # url: str = f"postgresql://user:secret@localhost:5432/gxshakezz"
 # url: str = f"postgresql://user:secret@localhost:5432/gxshakezz?options=-csearch_path=shakespeare"
 
-class GxSession:
+class GxSessionTable:
     # keep state of the great_expectations session
     # TODO: make this a singleton or borg ?
 
@@ -16,3 +16,15 @@ class GxSession:
         self.context = ge.get_context()
         self.sql_datasource = self.context.sources.add_sql(name=datasource_name, connection_string=url)
         self.sql_table_asset = self.sql_datasource.add_table_asset(name=f"{table_name}_asset", table_name=table_name)
+
+
+class GxSession:
+    # keep state of the great_expectations session
+    # TODO: make this a singleton or borg ?
+
+    context: AbstractDataContext
+    sql_datasource: SQLDatasource
+
+    def __init__(self, url: str, datasource_name: str, table_name: str):
+        self.context = ge.get_context()
+        self.sql_datasource = self.context.sources.add_sql(name=datasource_name, connection_string=url)
