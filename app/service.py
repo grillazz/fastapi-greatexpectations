5b8a50e9f373d8bd920e874aebd173f72ba1d1ff
Jakub Miazek <the@grillazz.com>
2023-05-17 21:50:59 +0200

    wip gx session service

--- app/service.py
+++ app/service.py
@@ -7,6 +7,7 @@ from great_expectations.data_context.data_context.abstract_data_context import A
 class GxSessionTable:
     # keep state of the great_expectations session
     # TODO: make this a singleton or borg ?
+    # TODO:   ValueError: "chapter_asset" already exists > catch this and check if i.e. asset already exists
 
     context: AbstractDataContext
     sql_datasource: SQLDatasource
@@ -15,8 +16,10 @@ class GxSessionTable:
     def __init__(self, url: str, datasource_name: str, table_name: str):
         self.context = ge.get_context()
         self.sql_datasource = self.context.sources.add_sql(name=datasource_name, connection_string=url)
+        # try:
         self.sql_table_asset = self.sql_datasource.add_table_asset(name=f"{table_name}_asset", table_name=table_name)
-
+        # except ValueError:
+        #     self.sql_table_asset = self.sql_datasource.get_asset(f"{table_name}_asset")
 
 class GxSession:
     # keep state of the great_expectations session
@@ -24,7 +27,14 @@ class GxSession:
 
     context: AbstractDataContext
     sql_datasource: SQLDatasource
+    sql_table_asset: TableAsset
 
-    def __init__(self, url: str, datasource_name: str, table_name: str):
+    def __init__(self, url: str, datasource_name: str):
         self.context = ge.get_context()
         self.sql_datasource = self.context.sources.add_sql(name=datasource_name, connection_string=url)
+
+    def set_asset(self, table_name: str):
+        try:
+            self.sql_table_asset = self.sql_datasource.add_table_asset(name=f"{table_name}_asset", table_name=table_name)
+        except ValueError:
+            self.sql_table_asset = self.sql_datasource.get_asset(f"{table_name}_asset")
