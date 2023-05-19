65803122f179566f9ed017f41e7ab50b0b1aa752
Jakub Miazek <the@grillazz.com>
2023-05-19 13:13:53 +0200

    restart api with gx 0.16.2

--- app/api/v1/expectation.py
+++ app/api/v1/expectation.py
@@ -1,13 +1,5 @@
-from enum import Enum
-
-from fastapi import APIRouter, Body, Depends, Query, status, Request
+from fastapi import APIRouter, Request
 from great_expectations.exceptions.exceptions import InvalidExpectationConfigurationError
-from sqlalchemy.engine import Engine
-from sqlalchemy.orm import Session
-
-from app.database import get_db, get_db_session
-from app.models.expectation import ExpectationStore
-from app.schemas.expectation import ExpectationSuiteSchema
 from app.service import GxSessionTable, GxSession
 
 
@@ -38,6 +30,7 @@ def try_expectation(
     except InvalidExpectationConfigurationError as e:
         return e.__dict__
 
+
 @router.get("/list_available_expectation_types")
 def list_available_expectation_types(
 ):
@@ -51,48 +44,3 @@ def list_available_expectation_types(
     _validator = _gx_session.context.get_validator(datasource_name="my_gx", data_asset_name="chapter_asset")
 
     return _validator.list_available_expectation_types()
-
-#
-#
-# @router.post(
-#     "/add/{gx_func}",
-#     status_code=status.HTTP_201_CREATED,
-#     response_model=ExpectationSuiteSchema,
-# )
-# def add_expectation(
-#     gx_func: GxFuncModel,
-#     database_schema: str,
-#     schema_table: str,
-#     suite_name: str,
-#     sql_engine: Engine = Depends(get_db),
-#     sql_session: Session = Depends(get_db_session),
-#     gx_mapping: dict = Body(
-#         None,
-#         description="pass parameters as json dict and in next step unpack to **mapping",
-#     ),
-# ):
-#     data_set = SqlAlchemyDataset(
-#         table_name=schema_table, engine=sql_engine, schema=database_schema
-#     )
-#
-#     # TODO: if suite for name already exists in database get it and update ?
-#     data_set.expectation_suite_name = suite_name
-#     try:
-#         eval(f"data_set.{gx_func.value}(**gx_mapping)")
-#     except TypeError as e:
-#         return e.__repr__()
-#     # TODO: if suite exist db.append_expectation() and update existing suite in database
-#     gx_suite = data_set.get_expectation_suite(discard_failed_expectations=False)
-#     expectation = ExpectationStore(
-#         suite_name=suite_name, suite_desc="", value=gx_suite.to_json_dict()
-#     )
-#     expectation.save(sql_session)
-#     return expectation
-#
-#
-# @router.get("", response_model=ExpectationSuiteSchema)
-# async def get_expectation(
-#     suite_name: str,
-#     sql_session: Session = Depends(get_db_session),
-# ):
-#     return ExpectationStore.find_by_name(sql_session, suite_name)
