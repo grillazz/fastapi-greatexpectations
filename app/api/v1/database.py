cd0c9c4b72ae30856c9854ab3f83da47fa7c6ff6
Jakub Miazek <the@grillazz.com>
2023-05-19 13:13:53 +0200

    restart api with gx 0.16.2

--- app/api/v1/database.py
+++ app/api/v1/database.py
@@ -22,17 +22,6 @@ async def get_schema_tables(
     return inspected.get_table_names(schema=sql_db_schema)
 
 
-# @router.get("/columns")
-# async def get_table_columns(
-#     database_schema: str,
-#     schema_table: str,
-#     sql_engine: Engine = Depends(get_db),
-# ) -> list[dict]:
-#     # TODO: add try catch when i.e. table not exists
-#     db = SqlAlchemyDataset(
-#         table_name=schema_table, engine=sql_engine, schema=database_schema
-#     )
-#     return db.columns
 @router.post("/columns/{datasource}/{table}")
 def get_table_columns(
     datasource: str,
