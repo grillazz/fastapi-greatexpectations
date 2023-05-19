6c10fb9306ef533cbabe0866866eadf1958591bf
Jakub Miazek <the@grillazz.com>
2023-05-19 14:30:35 +0200

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 
+from app.config import settings
 from app.main import app
+from app.service import GxSession
 
 
 @pytest.fixture
@@ -12,4 +14,5 @@ def client() -> Generator:
         app=app,
         base_url="http://testserver",
     ) as test_client:
+        app.state.gx = GxSession(settings.pg_url.__str__(), "my_gx")
         yield test_client
