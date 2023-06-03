52c7ff24567567e3da5164b33458fed1c501ec3e
Jakub Miazek <the@grillazz.com>
2023-06-03 20:44:26 +0200

    switch to GxSession class

--- app/api/v1/expectation.py
+++ app/api/v1/expectation.py
@@ -2,11 +2,24 @@ from fastapi import APIRouter, Request
 from great_expectations.exceptions.exceptions import (
     InvalidExpectationConfigurationError,
 )
-from app.config import settings
-from app.service import GxSessionTable, GxSession
-
 
 router = APIRouter(prefix="/v1/expectation")
+
+
+@router.get("/list_available_expectation_types/{datasource}/{table}")
+def list_available_expectation_types(datasource: str, table: str, request: Request):
+    _gx = request.app.state.gx
+
+    _gx.set_asset(table_name=table)
+
+    _validator = _gx.context.get_validator(
+        datasource_name=datasource, data_asset_name=_gx.sql_table_asset.name
+    )
+
+    try:
+        return eval(f"_validator.list_available_expectation_types()")
+    except InvalidExpectationConfigurationError as e:
+        return e.__dict__
 
 
 @router.post("/try_expectation/{datasource}/{table}/{expectation_type}")
@@ -25,18 +38,3 @@ def try_expectation(
         return eval(f"_validator.{expectation_type}()")
     except InvalidExpectationConfigurationError as e:
         return e.__dict__
-
-
-@router.get("/list_available_expectation_types")
-def list_available_expectation_types():
-    # TODO: pass gx session as dependency or if not possible create new one
-    datasource_name = "my_gx"
-    table_name = "chapter"
-    data_asset_name = "my_gx_asset"
-    _gx_session = GxSessionTable(settings.pg_url.__str__()+"?options=-csearch_path=shakespeare", datasource_name, table_name)
-
-    _validator = _gx_session.context.get_validator(
-        datasource_name="my_gx", data_asset_name="chapter_asset"
-    )
-
-    return _validator.list_available_expectation_types()
