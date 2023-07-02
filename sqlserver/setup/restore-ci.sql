0892e02406e253c338cd6591b26841e6cb0ba57f
Jakub Miazek <the@grillazz.com>
2023-07-02 17:07:19 +0200

    update github ci workflow for sqlserver - sqlcmd restore from bak

--- sqlserver/setup/restore-ci.sql
+++ sqlserver/setup/restore-ci.sql
@@ -6,7 +6,7 @@ Restore a database from a backup file
 CREATE DATABASE AdventureWorksLT2022;
 GO
 
-RESTORE DATABASE AdventureWorksLT2022 FROM DISK = '/var/opt/mssql/backup/AdventureWorksLT2022.bak'
+RESTORE DATABASE AdventureWorksLT2022 FROM DISK = 'AdventureWorksLT2022.bak'
     WITH REPLACE,
         MOVE 'AdventureWorksLT2022_Data' TO '/var/opt/mssql/data/AdventureWorksLT2022.mdf',
         MOVE 'AdventureWorksLT2022_log' TO '/var/opt/mssql/data/AdventureWorksLT2022_log.ldf';
