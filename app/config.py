c7b4c6edb94f294be5c88c90f6ca8efef2b85dd1
Jakub Miazek <the@grillazz.com>
2023-10-02 13:32:40 +0200

    fix config.py

--- app/config.py
+++ app/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
         password=os.getenv("MSSQL_SA_PASSWORD"),
         host=os.getenv("MSSQL_HOST"),
         port=1433,
-        path=f"/{os.getenv('MSSQL_DB') or ''}",
+        path=f"{os.getenv('MSSQL_DB') or ''}",
         query="driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes",
     )
     sql_datasource_name: str = os.getenv("SQL_DATASOURCE_NAME", "default")
